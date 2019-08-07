1ba5a49a125476575769deaf7c320afaacf3a029
marko1996dj <marko96djuric@gmail.com>
2019-08-07 14:59:09 +0200

    Updates

---
+++ precache-manifest.e9451a192d1031e31dee2d4a6e42a011.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f349283aad29237c24a4f972305d407a",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "170847f8189661e3d01d",
+    "url": "/weather-app/static/css/main.066fcc1b.chunk.css"
+  },
+  {
+    "revision": "2a1f5a1c881cbf273855",
+    "url": "/weather-app/static/js/2.5f0381c8.chunk.js"
+  },
+  {
+    "revision": "170847f8189661e3d01d",
+    "url": "/weather-app/static/js/main.0f175709.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  }
+]);
